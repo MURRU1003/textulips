9c7ebaf419a1b33668993cd9688a82706ea7c210
murru1003 <muradkureshi16@gmail.com>
2026-08-08 20:35:35 +0530

    Clean deployment without router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -39,16 +39,16 @@ function App() {
   return (
     <>
       {/* 1. Entire Navbar & Routes must be inside Router */}
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextApp" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About mode={mode} />} />
-            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
-          </Routes>
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About mode={mode} />} /> */}
+            <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+          {/* </Routes> */}
         </div>
-      </Router>
+      {/* // </Router> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <a className="navbar-brand" href="#"> {props.title} </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
         aria-label="Toggle navigation">
@@ -15,11 +15,11 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" to="/">Home</Link>
+              <a className="nav-link active" href="#">Home</a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link active" to="/about">About</Link>
-            </li>
+            </li> */}
           </ul>
 
           {/* Switch and Text Section */}
